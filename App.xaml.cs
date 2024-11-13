3aa638a9a2f291653e6bffadb47331f7fdbd5338
Gabriel B. Di Rezze <bortoletigabriel.drz@gmail.com>
2024-11-12 21:11:30 -0300

    Adicionar arquivos de projeto.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiGerenteDeEventos
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace MauiGerenteDeEventos
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        
+    }
+
+}
