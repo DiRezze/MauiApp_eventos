48be226d144034cf773012e359aa51dd13b73c8f
Gabriel B. Di Rezze <bortoletigabriel.drz@gmail.com>
2024-11-13 09:39:10 -0300

    fix: add NavigationPage to MainPage

--- App.xaml.cs
+++ App.xaml.cs
@@ -8,7 +8,7 @@ namespace MauiGerenteDeEventos
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            MainPage = new NavigationPage(new MainPage());
         }
     }
 }
