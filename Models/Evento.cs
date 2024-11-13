da5e9b146489bcfb3d116ac3b532352ffa4884c8
Gabriel B. Di Rezze <bortoletigabriel.drz@gmail.com>
2024-11-13 09:14:40 -0300

    created: Evento.cs model

---
+++ Models/Evento.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiGerenteDeEventos.Models
+{
+    public class Evento
+    {
+        public required string nome { get; set; }
+        public DateTime dataInicio { get; set; }
+        public DateTime dataTermino { get; set;}
+        public int numeroParticipantes { get; set; }
+        public string local { get; set; }
+        public decimal custoPorParticipante { get; set; }
+
+        public TimeSpan duracaoEmDias => dataTermino - dataInicio;
+
+        public decimal custoTotal => numeroParticipantes * custoPorParticipante;
+
+    }
+}
