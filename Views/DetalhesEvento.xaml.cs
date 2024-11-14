4c047247297f4fc45fedab8b85cb7642debdd2a9
Gabriel B. Di Rezze <bortoletigabriel.drz@gmail.com>
2024-11-14 08:53:01 -0300

    DetalhesEvento code behind

--- Views/DetalhesEvento.xaml.cs
+++ Views/DetalhesEvento.xaml.cs
@@ -7,6 +7,25 @@ public partial class DetalhesEvento : ContentPage
 	public DetalhesEvento(Evento eventoExibido)
 	{
 		InitializeComponent();
-	}
 
+        nomeLabel.Text = $"Nome do Evento: {eventoExibido.nome}";
+        localLabel.Text = $"Local: {eventoExibido.local}";
+
+        dataInicioLabel.Text = $"Data de Início: {eventoExibido.dataInicio:dd/MM/yyyy}";
+        dataTerminoLabel.Text = $"Data de Término: {eventoExibido.dataTermino:dd/MM/yyyy}";
+
+        string unidadeDuracao = eventoExibido.duracaoEmDias > TimeSpan.FromDays(1) ? "dias" : "dia";
+
+        duracaoLabel.Text = $"Duração do Evento: {eventoExibido.duracaoEmDias.Days} {unidadeDuracao}"; 
+
+        participantesLabel.Text = $"Número de Participantes: {eventoExibido.numeroParticipantes}";
+        custoLabel.Text = $"Custo por Participante: {eventoExibido.custoPorParticipante:C}";
+        custoTotalLabel.Text = $"Custo Total do Eventoo: {eventoExibido.custoTotal:C}";
+
+    }
+
+    private async void retornar_Clicked(object sender, EventArgs e)
+    {
+        await Navigation.PopToRootAsync();
+    }
 }
