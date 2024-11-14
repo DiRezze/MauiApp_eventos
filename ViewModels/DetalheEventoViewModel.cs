e8dff7cdff8ff5192f5a55f8ca7b9929fe1311db
Gabriel B. Di Rezze <bortoletigabriel.drz@gmail.com>
2024-11-14 07:46:57 -0300

    created: DetalheEventoViewModel

--- ViewModels/DetalheEventoViewModel.cs
+++ ViewModels/DetalheEventoViewModel.cs
@@ -1,27 +1,63 @@
 ﻿using MauiGerenteDeEventos.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MauiGerenteDeEventos.ViewModels
 {
-    public class DetalheEventoViewModel
+    // Não vou mentir, eu fiz isso aqui com IA
+    public class CadastroEventosViewModel : INotifyPropertyChanged
     {
-        private Evento _evento;
+        public event PropertyChangedEventHandler PropertyChanged;
 
-        public Evento Evento
+        private string nome;
+        private string local;
+        private DateTime dataInicio;
+        private DateTime dataTermino;
+        private int numeroParticipantes;
+        private decimal custoPorParticipante;
+
+        public string Nome
         {
-            get => _evento;
-            set.OnPropertyChanged()
-            {
-                _evento = value;
-                OnPropertyChanged();
-            }
+            get => nome;
+            set { nome = value; OnPropertyChanged(nameof(Nome)); }
         }
 
+        public string Local
+        {
+            get => local;
+            set { local = value; OnPropertyChanged(nameof(Local)); }
+        }
 
+        public DateTime DataInicio
+        {
+            get => dataInicio;
+            set { dataInicio = value; OnPropertyChanged(nameof(DataInicio)); }
+        }
 
+        public DateTime DataTermino
+        {
+            get => dataTermino;
+            set { dataTermino = value; OnPropertyChanged(nameof(DataTermino)); }
+        }
+
+        public int NumeroParticipantes
+        {
+            get => numeroParticipantes;
+            set { numeroParticipantes = value; OnPropertyChanged(nameof(NumeroParticipantes)); }
+        }
+
+        public decimal CustoPorParticipante
+        {
+            get => custoPorParticipante;
+            set { custoPorParticipante = value; OnPropertyChanged(nameof(CustoPorParticipante)); }
+        }
+
+        protected virtual void OnPropertyChanged(string propertyName) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
+
 }
